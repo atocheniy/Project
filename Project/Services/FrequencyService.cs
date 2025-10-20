e1e76d9c0c5aeb2b37e69b65d40428f7e45bf615
vshanovskiy <viktor6998@gmail.com>
2025-10-20 19:31:58 +0300

    create FrequencyService class in services

---
+++ Project/Services/FrequencyService.cs
@@ -0,0 +1,22 @@
+﻿using Project.Models;
+using System.Text.RegularExpressions;
+
+namespace Project.Services
+{
+    public class FrequencyService
+    {
+        public List<FrequencyResult> Analyze(string text)
+        {
+            var words = Regex.Matches(text.ToLower(), @"\b\w+\b")
+                             .Cast<Match>()
+                             .Select(m => m.Value);
+
+            var frequency = words.GroupBy(w => w)
+                                 .Select(g => new FrequencyResult { Word = g.Key, Count = g.Count() })
+                                 .OrderByDescending(f => f.Count)
+                                 .ToList();
+
+            return frequency;
+        }
+    }
+}
