ac6e251b1a108cdd7c753e0fb9049ea4a747555c
EinSof2718 <mmaxmoroz@mail.ru>
2025-10-20 20:02:23 +0300

    Upload tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
-﻿namespace Tests
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Tests
 {
     public class Tests
     {
@@ -7,10 +10,27 @@
         {
         }
 
+        //Обработка пустого текста
         [Test]
         public void Test1()
         {
-            Assert.Pass();
-        }
+            var words = dictionary.ExtractWords("");
+            Assert.That(words.Count(), Is.EqualTo(0));
+        }//Test1
+
+        //Обработка пустой папки
+        [Test]
+        public void Test2()
+        {
+            Assert.Throws<FileNotFoundException>(() => dictionary.Create("C:\\Новая папка"));
+        }//Test2
+
+        //Обработка несуществующей папки
+        [Test]
+        public void Test3()
+        {
+            Assert.Throws<DirectoryNotFoundException>(() => dictionary.Create("C:\\Новая папка_2"));
+        }//Test3
+
     }
 }
